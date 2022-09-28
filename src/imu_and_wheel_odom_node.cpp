3d0b6cb52ef7fab50cdd83fa149c9d88defd9d3d
DomBaril <dominic.baril@usherbrooke.ca>
2022-09-28 17:42:03 -0400

    a little fewer hair on my head

--- src/imu_and_wheel_odom_node.cpp
+++ src/imu_and_wheel_odom_node.cpp
@@ -2,62 +2,98 @@
 // Licence: BSD
 // Laval University, NORLAB, 2019
 
-#include "ros/ros.h"
-#include "tf/transform_broadcaster.h"
-#include "sensor_msgs/Imu.h"
-#include "nav_msgs/Odometry.h"
-#include "geometry_msgs/Quaternion.h"
-#include "geometry_msgs/Point.h"
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+//#include <tf2/tf2.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <geometry_msgs/msg/point.hpp>
 #include <cmath>
 #include <sstream>
 
 #define MISSED_ODOM_MSG_SAFETY_MULTIPLIER 6.0
 
-// frame names
-std::string p_odom_frame_;
-std::string p_base_frame_;
+class imuAndWheelOdomNode : public rclcpp::Node
+{
+public:
+    imuAndWheelOdomNode() :
+            Node("imu_and_wheel_odom_node")
+        {
+        //frame names
+        std::string p_odom_frame_;
+        std::string p_base_frame_;
 
-// tf stuff
-tf::TransformBroadcaster *tfB_;
-tf::StampedTransform transform_;
-tf::Quaternion tmp_;
-tf::Quaternion current_attitude;
-tf::Quaternion imu_alignment_;
-tf::Quaternion mag_north_correction_;
+        tfBroadcaster = std::unique_ptr<tf2_ros::TransformBroadcaster>(new tf2_ros::TransformBroadcaster(*this));
+
+        imuAndWheelOdomPublisher = this->create_publisher<nav_msgs::msg::Odometry>("odom_out", 2);
+
+        wheelOdomSubscription = this->create_subscription<nav_msgs::msg::Odometry>("wheel_odom_topic", 10,
+                                                                                         std::bind(
+                                                                                                 &imuAndWheelOdomNode::wheelOdomMsgCallback,
+                                                                                                 this,
+                                                                                                 std::placeholders::_1));
+
+        imuSubscription = this->create_subscription<sensor_msgs::msg::PointCloud2>("imu_topic", 10,
+                                                                                   std::bind(
+                                                                                           &imuAndWheelOdomNode::imuMsgCallback,
+                                                                                           this,
+                                                                                           std::placeholders::_1));
+        }
+
+private:
+    //tf stuff
+    tf2::StampedTransform transform_;
+    tf2::Quaternion tmp_;
+    tf2::Quaternion current_attitude;
+    tf2::Quaternion imu_alignment_;
+    tf2::Quaternion mag_north_correction_;
 
 
+    std::vector<double> imu_alignment_rpy_(
 
-std::vector<double> imu_alignment_rpy_(3, 0.0);
-double mag_north_correction_yaw_ = 0;
+    3, 0.0);
 
-// output odom stuff
-bool p_publish_odom_;
-std::string p_odom_topic_name_;
-ros::Publisher *odom_pub_;
-nav_msgs::Odometry odom_msg_;
+    bool p_publish_odom_;
+    std::string p_odom_topic_name_;
+//        ros::Publisher *odom_pub_;
+    nav_msgs::Odometry odom_msg_;
 
-// input wheel odom stuff
-tf::Vector3 current_position(0.0,0.0,0.0);
-tf::Vector3 current_linear_vel(0.0,0.0,0.0);
+    tf2::Vector3 current_position(
 
-bool initial_wheel_odom_received = false;
-ros::Time previous_w_odom_stamp;
-double p_wheel_odom_vx_scale = 1.0;
-double p_longest_expected_input_odom_period = MISSED_ODOM_MSG_SAFETY_MULTIPLIER/20.0;
+    0.0,0.0,0.0);
 
-bool p_allow_translation = true;
+    tf2::Vector3 current_linear_vel(
 
-#ifndef TF_MATRIX3x3_H
-typedef btScalar tfScalar;
-namespace tf { typedef btMatrix3x3 Matrix3x3; }
-#endif
+    0.0,0.0,0.0);
 
+
+    double mag_north_correction_yaw_ = 0;
+
+    // input wheel odom stuff
+    bool initial_wheel_odom_received = false;
+    rclcpp::Time previous_w_odom_stamp;
+    double p_wheel_odom_vx_scale = 1.0;
+    double p_longest_expected_input_odom_period = MISSED_ODOM_MSG_SAFETY_MULTIPLIER / 20.0;
+
+    // output odom stuff
+    bool p_allow_translation = true;
+
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tfBroadcaster;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr imuAndWheelOdomPublisher;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr wheelOdomSubscription;
+
+//#ifndef TF_MATRIX3x3_H
+//    typedef btScalar tfScalar;
+//namespace tf { typedef btMatrix3x3 Matrix3x3; }
+//#endif
 
 void imuMsgCallback(const sensor_msgs::Imu &imu_msg) {
     tf::quaternionMsgToTF(imu_msg.orientation, tmp_);
 
-    if (std::isnan(tmp_.getX()) || std::isnan(tmp_.getY()) || std::isnan(tmp_.getZ()) || std::isnan(tmp_.getW()))
-    {
+    if (std::isnan(tmp_.getX()) || std::isnan(tmp_.getY()) || std::isnan(tmp_.getZ()) || std::isnan(tmp_.getW())) {
         ROS_WARN("Received IMU message with NaN values, dropping");
         return;
     }
@@ -67,7 +103,7 @@ void imuMsgCallback(const sensor_msgs::Imu &imu_msg) {
 
     current_attitude = tmp_;
     transform_.setRotation(current_attitude);
-    transform_.setOrigin(tf::Vector3(current_position.x(),current_position.y(),current_position.z()));
+    transform_.setOrigin(tf::Vector3(current_position.x(), current_position.y(), current_position.z()));
     transform_.stamp_ = imu_msg.header.stamp;
 
     tfB_->sendTransform(transform_);
@@ -88,7 +124,7 @@ void imuMsgCallback(const sensor_msgs::Imu &imu_msg) {
         odom_msg_.header.stamp = imu_msg.header.stamp;
         odom_pub_->publish(odom_msg_);
     }
-}
+    }
 
 void wheelOdomMsgCallback(const nav_msgs::Odometry &wheel_odom_msg) {
 
@@ -96,47 +132,49 @@ void wheelOdomMsgCallback(const nav_msgs::Odometry &wheel_odom_msg) {
     tf::Vector3 new_position;
 
     //check for nonsense data
-    if(std::isnan(wheel_odom_msg.twist.twist.linear.x) ||
-       std::isnan(wheel_odom_msg.twist.twist.linear.y) ||
-       std::isnan(wheel_odom_msg.twist.twist.linear.z))
-    {
+    if (std::isnan(wheel_odom_msg.twist.twist.linear.x) ||
+        std::isnan(wheel_odom_msg.twist.twist.linear.y) ||
+        std::isnan(wheel_odom_msg.twist.twist.linear.z)) {
         ROS_WARN("Received Wheel Odometry message with NaN values, dropping");
         return;
     }
 
     // to know our delta time step, we need the previous time stamp. The first odom message is used to initialize it
-    if (!initial_wheel_odom_received){
+    if (!initial_wheel_odom_received) {
         previous_w_odom_stamp = wheel_odom_msg.header.stamp;
         initial_wheel_odom_received = true;
         return;
     }
 
     // compute and store new position if asked for
-    if(p_allow_translation) {
+    if (p_allow_translation) {
 
         // body to world rotation
         tf::Transform rotation_body_to_world;
-        rotation_body_to_world.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );      // no translation
-        rotation_body_to_world.setRotation( current_attitude );                          // current orientation
+        rotation_body_to_world.setOrigin(tf::Vector3(0.0, 0.0, 0.0));      // no translation
+        rotation_body_to_world.setRotation(current_attitude);                          // current orientation
 
 
         // express the velocity in the world frame
-        tf::Vector3 velocity_in_world = rotation_body_to_world * tf::Vector3(wheel_odom_msg.twist.twist.linear.x*p_wheel_odom_vx_scale,
-                                                                             wheel_odom_msg.twist.twist.linear.y,
-                                                                             wheel_odom_msg.twist.twist.linear.z);
-                                                                             // for warthog, only x is expected non-zero
+        tf::Vector3 velocity_in_world =
+                rotation_body_to_world * tf::Vector3(wheel_odom_msg.twist.twist.linear.x * p_wheel_odom_vx_scale,
+                                                     wheel_odom_msg.twist.twist.linear.y,
+                                                     wheel_odom_msg.twist.twist.linear.z);
+        // for warthog, only x is expected non-zero
 
         // time increment
         double delta_t = (wheel_odom_msg.header.stamp - previous_w_odom_stamp).toSec();
 
-        if (delta_t < 1e-7){
-            ROS_WARN("Received wheel odometry message with negative or zero time increment, ignoring that one and starting from the next one.");
+        if (delta_t < 1e-7) {
+            ROS_WARN(
+                    "Received wheel odometry message with negative or zero time increment, ignoring that one and starting from the next one.");
             initial_wheel_odom_received = false;
             return;
         }
 
-        if (delta_t >= p_longest_expected_input_odom_period){
-            ROS_WARN("Received wheel odometry message with too much delay after the previous one. Ignoring that and starting from new.");
+        if (delta_t >= p_longest_expected_input_odom_period) {
+            ROS_WARN(
+                    "Received wheel odometry message with too much delay after the previous one. Ignoring that and starting from new.");
             initial_wheel_odom_received = false;
             return;
         }
@@ -149,7 +187,7 @@ void wheelOdomMsgCallback(const nav_msgs::Odometry &wheel_odom_msg) {
 
         // update the current position and linear velocity
         current_position = new_position;
-        current_linear_vel = tf::Vector3(wheel_odom_msg.twist.twist.linear.x*p_wheel_odom_vx_scale,
+        current_linear_vel = tf::Vector3(wheel_odom_msg.twist.twist.linear.x * p_wheel_odom_vx_scale,
                                          wheel_odom_msg.twist.twist.linear.y,
                                          wheel_odom_msg.twist.twist.linear.z);
 
@@ -157,6 +195,14 @@ void wheelOdomMsgCallback(const nav_msgs::Odometry &wheel_odom_msg) {
 
     previous_w_odom_stamp = wheel_odom_msg.header.stamp;
 }
+
+}
+};
+
+
+
+
+
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, ROS_PACKAGE_NAME);
@@ -239,8 +285,7 @@ int main(int argc, char **argv) {
 
 
     // Subscribe the IMU and start the loop
-    ros::Subscriber wheel_odom_subscriber = n.subscribe("wheel_odom_topic", 10, wheelOdomMsgCallback);
-    ros::Subscriber imu_subscriber = n.subscribe("imu_topic", 10, imuMsgCallback);
+
 
     ros::spin();
 
