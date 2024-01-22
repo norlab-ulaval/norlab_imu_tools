ac66b867f48e7b7c55f4ebe973a1ac3330937107
WilliamDubois <william.dubois@usherbrooke.ca>
2024-01-22 09:46:02 -0500

    added a second altitude formula

--- src/altitude_computation_node.cpp
+++ src/altitude_computation_node.cpp
@@ -1,5 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/fluid_pressure.hpp>
+#include <sensor_msgs/msg/temperature.hpp>
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <cmath>
 #include <sstream>
@@ -15,6 +16,9 @@ public:
         pressureIn = this->create_subscription<sensor_msgs::msg::FluidPressure>("pressure_in", 10,
                                                                                 std::bind(&altitudeComputation::pressureMsgCallback, this,
                                                                                           std::placeholders::_1));
+        tempIn = this->create_subscription<sensor_msgs::msg::Temperature>("temp_in", 10,
+                                                                                std::bind(&altitudeComputation::tempMsgCallback, this,
+                                                                                          std::placeholders::_1));
         altitudePub = this->create_publisher<geometry_msgs::msg::PointStamped>("altitude_out", 10);
 
         this->declare_parameter<std::string>("formula", "barometric");
@@ -28,17 +32,27 @@ private:
     double g = 9.80665;
     double R = 8.3144598;
     double M = 0.0289644;
+    double P0 = 101325.0;
 
     bool is_first_altitude = true;
+    bool is_first_msg = true;
+
     double initial_altitude = 0.0;
 
     std::string formula;
+    sensor_msgs::msg::Temperature lastTempMeasurement;
+    std::mutex lastTempMutex;
 
     rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr altitudePub;
     rclcpp::Subscription<sensor_msgs::msg::FluidPressure>::SharedPtr pressureIn;
+    rclcpp::Subscription<sensor_msgs::msg::Temperature>::SharedPtr tempIn;
 
     void pressureMsgCallback(const sensor_msgs::msg::FluidPressure &pressure_msg)
     {
+        if (this->is_first_msg)
+        {
+            this->P0 = pressure_msg.fluid_pressure;
+        }
         double P = pressure_msg.fluid_pressure;
         double altitude = 0;
         if (this->formula == "barometric")
@@ -48,13 +62,15 @@ private:
         }
         else if (this->formula == "hypsometric")
         {
+            // assuming the virtual temperature is the temperature measured by the dps sensor.
             double exponent_part = std::pow(P/this->Pb, (this->R*this->Lb)/(this->g*this->M));
             altitude = this->hb - ((this->Tb/this->Lb)*(exponent_part - 1));
         }
         else
         {
-            double exponent_part = std::pow(P/this->Pb, (this->R*this->Lb)/(this->g*this->M));
-            altitude = this->hb - ((this->Tb/this->Lb)*(exponent_part - 1));
+            this->lastTempMutex.lock();
+            altitude = ((this->R * this->lastTempMeasurement.temperature)/this->g) * std::log(this->P0/P);
+            this->lastTempMutex.unlock();
         }
         if (this->is_first_altitude)
         {
@@ -65,8 +81,21 @@ private:
     	output_msg.header = pressure_msg.header;
     	output_msg.point.x = 0.0;
     	output_msg.point.y = 0.0;
-    	output_msg.point.z = altitude - this->initial_altitude;
+        if (this->formula == "hypsometric")
+        {
+            output_msg.point.z = altitude;
+        }
+        else
+        {
+            output_msg.point.z = altitude - this->initial_altitude;
+        }
     	altitudePub->publish(output_msg);
+    }
+    void tempMsgCallback(const sensor_msgs::msg::Temperature &temp_msg)
+    {
+        this->lastTempMutex.lock();
+        this->lastTempMeasurement = msg;
+        this->lastTempMutex.unlock();
     }
 };
 
